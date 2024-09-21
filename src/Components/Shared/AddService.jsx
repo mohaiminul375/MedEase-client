ad50c475c7f9310e234a12ab9d9a29eadb6e5168
Mohaiminul375 <mohaiminul375@gmail.com>
2024-09-21 15:50:00 +0600

    service added successfully

---
+++ src/Components/Shared/AddService.jsx
@@ -0,0 +1,111 @@
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import axios from "axios";
+import React from "react";
+import { useForm } from "react-hook-form";
+import { FaXmark } from "react-icons/fa6";
+import { IoMdAdd } from "react-icons/io";
+import Swal from "sweetalert2";
+const AddService = () => {
+  const queryClient = useQueryClient();
+
+  const { mutateAsync } = useMutation({
+    mutationFn: async (service) => {
+      console.log(service);
+      const { data } = await axios.post(
+        "http://localhost:3000/services",
+        service
+      );
+
+        console.log(data);
+      return data;
+    },
+    onSuccess: (data) => {
+      if (data.insertedId) {
+        Swal.fire({
+          title: "Successful",
+          text: "Service added",
+          icon: "success",
+        });
+      }
+    },
+  });
+
+  // react hook form
+  const { register, handleSubmit } = useForm();
+
+  const onSubmit = (service) => {
+    console.log(service);
+    mutateAsync(service);
+  };
+  return (
+    <section>
+      <div className="flex justify-center">
+        <button
+          onClick={() => document.getElementById("form_modal").showModal()}
+          className="px-5 py-2 bg-[#0155BD] text-white font-semibold rounded-full flex items-center text-xl"
+        >
+          <IoMdAdd className="text-xl" /> Add Service
+        </button>
+      </div>
+
+      <dialog id="form_modal" className="modal">
+        <div className="modal-box">
+          <h2 className="text-center font-bold text-xl text-[#0155BD]">
+            Add Service
+          </h2>
+          <div className="modal-action">
+            <form method="dialog">
+              {/* if there is a button in form, it will close the modal */}
+              <button className="rounded-full bg-red-800 text-white p-1 font-bold">
+                <FaXmark />
+              </button>
+            </form>
+          </div>
+          <form onSubmit={handleSubmit(onSubmit)}>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Service Name</span>
+              </label>
+              <input
+                type="text"
+                placeholder="input service name"
+                className="input input-bordered"
+                {...register("service-name")}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <input
+                type="text"
+                placeholder="input description"
+                className="input input-bordered"
+                {...register("description")}
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Price</span>
+              </label>
+              <input
+                type="text"
+                placeholder="input service price"
+                className="input input-bordered"
+                {...register("price")}
+                required
+              />
+            </div>
+            <button className="mt-5 w-full py-2 bg-[#0155BD] text-white font-semibold rounded-full">
+              Add
+            </button>
+          </form>
+        </div>
+      </dialog>
+    </section>
+  );
+};
+
+export default AddService;
--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import { router } from './Router/Routes.jsx'
-
-createRoot(document.getElementById('root')).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import { router } from "./Router/Routes.jsx";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from "@tanstack/react-query";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </StrictMode>
+);
